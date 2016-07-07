9b4bc9185bed048883e69009a56d15ecb75fa09b
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-06 20:02:40 -0500

    First Commit

---
+++ config/settings/sandbox.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .base import *
+
+import dj_database_url
+
+# DEBUG
+# -----------------------------------------------------------------------------
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
