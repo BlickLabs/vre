caa99ef93586b0dd5f10a16967e5dbbbbba5f794
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-08 13:07:46 -0500

    Add documents to admin

---
+++ vre/apps/documents/admin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title',)
