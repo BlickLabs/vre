898639b7f6d838d0db487412346204e76c96af8e
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-09 10:20:57 -0500

    Add admin to django admin

---
+++ vre/apps/users/admin.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from vre.apps.users.forms import UserChangeForm, UserCreationForm
+from vre.apps.users.models import CoolUser
+
+
+@admin.register(CoolUser)
+class UserAdmin(BaseUserAdmin):
+    # The forms.py to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'first_name', 'last_name', 'user_type',
+                    'is_active')
+    list_filter = ('user_type',)
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password', 'user_type')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_active')
+        }),
+        ('Develops', {
+            'fields': ('develops',)
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'user_type',
+                       'password1', 'password2', 'develops')}),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.unregister(Group)
