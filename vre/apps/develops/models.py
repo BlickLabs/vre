54ece393d9af6df7f3ffd72d79ef50c400f538e1
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-09 12:40:01 -0500

    Add slug to develop model

--- vre/apps/develops/models.py
+++ vre/apps/develops/models.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*
 
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -11,11 +12,22 @@ class Develop(models.Model):
         max_length=50,
         blank=False,
         null=False,
+        unique=True,
+    )
+
+    slug = models.SlugField(
+        _('Slug'),
+        max_length=50,
+        unique=True,
     )
 
     class Meta:
         verbose_name = _('Develop')
         verbose_name_plural = _('Develops')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Develop, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
