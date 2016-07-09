e500c80edd58be715d68c6afb273e2976c74f14b
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-07-09 12:41:33 -0500

    Change develop form from django admin

--- vre/apps/develops/admin.py
+++ vre/apps/develops/admin.py
@@ -3,9 +3,11 @@
 
 from django.contrib import admin
 
+from vre.apps.develops.forms import DevelopForm
 from . import models
 
 
 @admin.register(models.Develop)
 class DevelopsAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    form = DevelopForm
