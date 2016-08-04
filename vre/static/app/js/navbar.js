12145db9484db9426aea3c44bf236ce48a3e8480
Alan Sanchez <sanchezpineda03@gmail.com>
2016-08-03 21:14:52 -0500

    Fixed minor bug in homepage navbar

--- vre/static/app/js/navbar.js
+++ vre/static/app/js/navbar.js
@@ -1,14 +1,14 @@
 (function () {
   'use strict';
   if ($('.homepage-cover').length) {
+    if ($(window).scrollTop() >= 30) {
+      $('.phone-box').addClass('fix-in-top');
+      $('.homepage-menu').addClass('fix-in-top');
+    } else {
+      $('.phone-box').removeClass('fix-in-top');
+      $('.homepage-menu').removeClass('fix-in-top');
+    }
     $(window).scroll(function () {
-      if ($(window).scrollTop() >= 30) {
-        $('.phone-box').addClass('fix-in-top');
-        $('.homepage-menu').addClass('fix-in-top');
-      } else {
-        $('.phone-box').removeClass('fix-in-top');
-        $('.homepage-menu').removeClass('fix-in-top');
-      }
     });
   }
 }());
