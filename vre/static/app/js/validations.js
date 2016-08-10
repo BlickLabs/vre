9b39fc9ce1c2cbf878ffcb7727ec78b700a0149e
Alan Sanchez <sanchezpineda03@gmail.com>
2016-08-09 21:46:00 -0500

    Datepicker is now in spanish

--- vre/static/app/js/validations.js
+++ vre/static/app/js/validations.js
@@ -1,6 +1,27 @@
 (function () {
   'use strict';
+  /*global moment */
+  moment.updateLocale('en', {
+    months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    weekdays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+    weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+    weekdaysMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+  });
+  moment().isoWeekday(1);
   $('#visit-us-datepicker').datetimepicker({
-    inline: true
+    format: 'DD/MM/YYYY',
+    inline: true,
+    icons: {
+      time: '',
+      date: '',
+      up: '',
+      down: '',
+      previous: '',
+      next: '',
+      today: '',
+      clear: '',
+      close: ''
+    }
   });
 }());
