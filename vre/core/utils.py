37abd722d59ce177e064487284384d7906ad52aa
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-08-17 16:38:11 -0500

    #7, Add export as xls function to the project core

--- vre/core/utils.py
+++ vre/core/utils.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 from django.core.mail import EmailMultiAlternatives
+from django.http import HttpResponse
 from django.template import loader
+
+from pyExcelerator import *
 
 DEFAULT_NOTIFICATION_EMAIL_FROM = "VRE - Notificaciones <postmaster@%s>" % (
     settings.MAILGUN_SERVER_NAME
@@ -22,3 +26,38 @@ def send_email(subject, body, to_email, context, from_email=None):
     )
     email_message.attach_alternative(body, "text/html")
     email_message.send()
+
+
+def export_as_xls(modeladmin, request, queryset):
+    """
+    Generic xls export admin action.
+    """
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = modeladmin.model._meta
+
+    wb = Workbook()
+    ws0 = wb.add_sheet('0')
+    col = 0
+    field_names = []
+    # write header row
+    for field in opts.fields:
+        ws0.write(0, col, field.name)
+        field_names.append(field.name)
+        col = col + 1
+
+    row = 1
+    # Write data rows
+    for obj in queryset:
+        col = 0
+        for field in field_names:
+            val = unicode(getattr(obj, field)).strip()
+            ws0.write(row, col, val)
+            col = col + 1
+        row = row + 1
+
+    wb.save('/tmp/output.xls')
+    response = HttpResponse(open('/tmp/output.xls','r').read(),
+                            content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=%s.xls' % unicode(opts).replace('.', '_')
+    return response
