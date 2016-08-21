97a366cc6343cae4533cc106159eca0ad281eeb8
Alan Sanchez <sanchezpineda03@gmail.com>
2016-08-20 21:29:26 -0500

    Added form validations

--- vre/static/app/js/validations.js
+++ vre/static/app/js/validations.js
@@ -1,7 +1,28 @@
 (function () {
   'use strict';
-  /*global moment */
+  /*global jQuery, moment */
   /*jslint sub:true*/
+
+  jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es obligatorio",
+    remote: "Please fix this field",
+    email: "Ingresa una dirección de correo válida",
+    url: "Ingresa una URL válida",
+    date: "Ingresa una fecha válida",
+    dateISO: "Ingresa una fecha válida (ISO)",
+    number: "Ingresa un número válido",
+    digits: "Solo se permiten números dígitos",
+    creditcard: "Ingresa un número de tarjeta válido",
+    equalTo: "Los valores deben coincidir",
+    accept: "Please enter a value with a valid extension",
+    maxlength: jQuery.validator.format("No ingreses más de {0} caracteres"),
+    minlength: jQuery.validator.format("Ingresa al menos {0} caracteres"),
+    rangelength: jQuery.validator.format("El texto debe tener entre {0} y {1} caracteres"),
+    range: jQuery.validator.format("Ingresa un valor entre {0} y {1}"),
+    max: jQuery.validator.format("Ingresa un valor menor o igual que {0}"),
+    min: jQuery.validator.format("Ingresa un valor mayor o igual que {0}")
+  });
+
   moment.updateLocale('en', {
     months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
     monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
@@ -28,5 +49,35 @@
   $('.visit-us-form').submit(function () {
     var dateValue = moment($('#visit-us-datepicker').data('DateTimePicker').date()['_d']);
     $('.visit-us-form #hidden_date').val(dateValue.format('DD/MM/YYYY'));
+  });
+
+  $('.login-form').validate({
+    rules: {
+      'username': {
+        required: true,
+        email: true
+      },
+      'password': {
+        required: true
+      }
+    }
+  });
+
+  $('.contact-form, .visit-us-form').validate({
+    rules: {
+      name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      phone: {
+        required: true
+      },
+      message: {
+        required: false
+      }
+    }
   });
 }());
