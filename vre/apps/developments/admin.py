f45462c83d946db43f98253818c9c731c22af265
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-08-20 22:39:43 -0500

    #21, Only show in develop the Develop Admin

--- vre/apps/developments/admin.py
+++ vre/apps/developments/admin.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*
 
 from django.contrib import admin
+from django.conf import settings
 
 from vre.apps.developments.forms import DevelopForm
 from vre.core.utils import export_as_xls
 from . import models
 
-
-@admin.register(models.Develop)
-class DevelopsAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    form = DevelopForm
-    actions = [export_as_xls]
-    export_as_xls.short_description = "Export selected objects to XLS"
+if settings.DEBUG:
+    @admin.register(models.Develop)
+    class DevelopsAdmin(admin.ModelAdmin):
+        list_display = ('name',)
+        form = DevelopForm
+        actions = [export_as_xls]
+        export_as_xls.short_description = "Export selected objects to XLS"
