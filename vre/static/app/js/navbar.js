da9d6e2df2dbd988bb20363341f2cc7cd0af5c1c
Alan Sanchez <sanchezpineda03@gmail.com>
2016-08-20 19:27:23 -0500

    Added responsve menu logic in homepage

--- vre/static/app/js/navbar.js
+++ vre/static/app/js/navbar.js
@@ -1,21 +1,39 @@
 (function () {
   'use strict';
-  if ($('.homepage-cover').length) {
+  var mobile = window.matchMedia('(max-width: 1100px)');
+
+  function detectTouch(e) {
+    if (!$(e.target).is('.homepage-cover .menu-list') && !$(e.target).closest('.homepage-cover .menu-list').length && !$(e.target).is('#navbar-trigger') && $('#navbar-trigger').prop('checked') === true) {
+      $('#navbar-trigger').prop('checked', false);
+    }
+  }
+
+  function checkScroll() {
     if ($(window).scrollTop() >= 30) {
       $('.phone-box').addClass('fix-in-top');
       $('.homepage-menu').addClass('fix-in-top');
+      if (mobile.matches) {
+        document.addEventListener('touchstart', detectTouch, false);
+      }
     } else {
       $('.phone-box').removeClass('fix-in-top');
       $('.homepage-menu').removeClass('fix-in-top');
+      document.removeEventListener('touchstart', detectTouch, false);
     }
-    $(window).scroll(function () {
-      if ($(window).scrollTop() >= 30) {
-        $('.phone-box').addClass('fix-in-top');
-        $('.homepage-menu').addClass('fix-in-top');
-      } else {
-        $('.phone-box').removeClass('fix-in-top');
-        $('.homepage-menu').removeClass('fix-in-top');
-      }
-    });
   }
+
+  function checkHomepage() {
+    if ($('.homepage-cover').length) {
+      checkScroll();
+      $(window).scroll(function () {
+        checkScroll();
+      });
+    }
+  }
+
+  checkHomepage();
+
+  $(window).resize(function () {
+    checkHomepage();
+  });
 }());
