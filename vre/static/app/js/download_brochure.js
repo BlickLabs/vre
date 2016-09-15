6e2cb995c846b256daf3a70d40343bec4f2ccd3f
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-15 00:45:28 -0500

    Disabled brochure while waiting ajax response.

--- vre/static/app/js/download_brochure.js
+++ vre/static/app/js/download_brochure.js
@@ -4,14 +4,17 @@ var addBrochureListener = (function () {
   return function (gaEventName, developmentID) {
     $('#download-brochure-button').click(function (e) {
       e.preventDefault();
+      var button = $(this);
       $.ajax({
         url: djangoURLs.suscribeToNewsletter,
         type : 'POST',
         data: $('#download-brochure-form').serialize(),
         beforeSend: function () {
           ga('send', 'event', gaEventName, 'ok');
+          button.addClass('disabled');
         },
         complete: function () {
+          button.removeClass('disabled');
           window.location.href = djangoURLs.downloadBrochure + developmentID;
         }
       });
