87a3681da55efce4821f290e3af9030d91bbaea1
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-09-14 19:06:26 -0500

    #35 Change logic on email sendig

--- vre/apps/newsletter/views.py
+++ vre/apps/newsletter/views.py
@@ -17,6 +17,7 @@ import urlparse
 class NewsletterView(View):
     def post(self, request):
         source = request.POST.get('source')
+        email = request.POST.get('email')
         list_id = self.get_list_id(request)
         endpoint = urlparse.urljoin(
             settings.MAILCHIMP_API_ROOT, 'lists/%s/members/' % list_id
@@ -29,38 +30,46 @@ class NewsletterView(View):
         response = requests.post(
             endpoint, auth=('apikey', settings.MAILCHIMP_API_KEY), data=data)
         d = json.loads(response.content)
-        subscriber = Subscriber(
-            email=request.POST.get('email'),
-            name=request.POST.get('name', None),
-            phone=request.POST.get('phone', None),
-            source=request.POST.get('source'),
-        )
-        subscriber.save()
+        if email:
+            try:
+                subscriber = Subscriber.objects.get(
+                    email=request.POST.get('email'),
+                    source=request.POST.get('source')
+                )
+            except Subscriber.DoesNotExist:
+                subscriber = Subscriber(
+                    email=request.POST.get('email'),
+                    name=request.POST.get('name', None),
+                    phone=request.POST.get('phone', None),
+                    source=request.POST.get('source'),
+                )
+                subscriber.save()
 
-        if source == 'indiana' or source == 'dakota':
-            context = {
-                'newsletter': 'False',
-                'brochure': 'True',
-                'brochure_title': source,
-                'email': request.POST.get('email')
-            }
-        else:
-            context = {
-                'newsletter': 'True',
-                'brochure': 'False',
-                'brochure_title': None,
-                'email': request.POST.get('email')
-            }
+            if source == 'indiana' or source == 'dakota':
+                context = {
+                    'newsletter': 'False',
+                    'brochure': 'True',
+                    'brochure_title': source,
+                    'email': request.POST.get('email')
+                }
+            else:
+                context = {
+                    'newsletter': 'True',
+                    'brochure': 'False',
+                    'brochure_title': None,
+                    'email': request.POST.get('email')
+                }
 
-        send_email(
-            subject='email/subjects/newsletter.txt',
-            body='email/newsletter.html',
-            from_email="VRE - Notificacion <postmaster@%s>" % (
-                settings.MAILGUN_SERVER_NAME
-            ),
-            to_email=[settings.DEFAULT_EMAIL_TO],
-            context=context
-        )
+            if d.get('status') == 'subscribed':
+                send_email(
+                    subject='email/subjects/newsletter.txt',
+                    body='email/newsletter.html',
+                    from_email="VRE - Notificacion <postmaster@%s>" % (
+                        settings.MAILGUN_SERVER_NAME
+                    ),
+                    to_email=[settings.DEFAULT_EMAIL_TO],
+                    context=context
+                )
         return JsonResponse(response.json())
 
     def get_list_id(self, request):
