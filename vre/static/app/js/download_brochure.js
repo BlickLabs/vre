80b3d688995184683877cb9710afbd7832c356d4
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-19 23:48:37 -0500

    This closes #36. Validated email before downloading brochure.

--- vre/static/app/js/download_brochure.js
+++ vre/static/app/js/download_brochure.js
@@ -4,17 +4,28 @@ var addBrochureListener = (function () {
   return function (gaEventName, developmentID) {
     $('#download-brochure-button').click(function (e) {
       e.preventDefault();
-      var button = $(this);
+      var button = $(this),
+        span = $('#download-brochure-span');
       $.ajax({
         url: djangoURLs.suscribeToNewsletter,
         type : 'POST',
         data: $('#download-brochure-form').serialize(),
         beforeSend: function () {
-          ga('send', 'event', gaEventName, 'ok');
-          button.addClass('disabled');
+          var isEmail = /^[\-a-z0-9~!$%\^&*_=+}{\'?]+(\.[\-a-z0-9~!$%\^&*_=+}{\'?]+)*@([a-z0-9_][\-a-z0-9_]*(\.[\-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+          if (isEmail.test($('#download-brochure-email').val())) {
+            button.addClass('disabled');
+            span.text('');
+            span.css('margin-top', '0');
+            ga('send', 'event', gaEventName, 'ok');
+          } else {
+            span.css('margin-top', '20px');
+            span.text('Introduce una dirección de correo válida.');
+            return false;
+          }
         },
         complete: function () {
           button.removeClass('disabled');
+          span.text('');
           window.location.href = djangoURLs.downloadBrochure + developmentID;
         }
       });
