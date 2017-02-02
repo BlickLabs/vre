ffbc8ce7907d4564c15c74db63e4040196fded3e
Alan Sanchez <sanchezpineda03@gmail.com>
2017-02-01 19:24:11 -0600

    This closes #70. Added div over 360 videos.

--- vre/static/app/js/homepage_developments_iframes.js
+++ vre/static/app/js/homepage_developments_iframes.js
@@ -6,4 +6,7 @@
       .siblings().removeClass('active');
     $('.container-360 iframe').attr('src', $(this).attr('data-iframe-url'));
   });
+  $('.help-360').click(function help360() {
+    $(this).remove();
+  });
 }());
