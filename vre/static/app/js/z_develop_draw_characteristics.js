44c9737f48dc5bf44e43c2c087840042090c8cf6
soycamis <camiilososa@gmail.com>
2018-03-05 13:00:08 -0600

    change of image, list of features, active element

--- vre/static/app/js/z_develop_draw_characteristics.js
+++ vre/static/app/js/z_develop_draw_characteristics.js
@@ -1,26 +1,71 @@
+// Obtiene el elemento padre de la lista de caracteristicas,
+// Para poder eliminar y agregar las nuevas caractetristicas segun el,
+// desarrollo y departamento seleccionado.
+var lista_de_caracteristicas = document.querySelector('.department-icons');
+
+var activar_elemento = function(el) {
+  var length             = el.parentNode.children.length;
+  var elementos_de_lista = el.parentNode.children;
+
+  for(var i = 0; i < length ; i++) {
+    if( elementos_de_lista[i].classList.contains('active') ) {
+      elementos_de_lista[i].classList.remove('active');
+    }
+    el.classList.add('active');
+  }
+};
+
+
 /*
-
-<li class="list-item">
-  <img src="{% static "img/vre/vre-metros.svg" %}" alt="" />
-  <p>230 m<sup>2</sup></p>
-</li>
+* Actualiza la lista y la imagen del plano.
 */
+var actualizar_caracteristicas = function(el, desarrollo) {
+  var lista_actual    = document.querySelector('.icons-list');
+  var plano           = document.querySelector('.department-img img');
+  var departamento    = el.dataset.id;
+  var info_desarrollo = departamentos_json.departamentos[desarrollo][departamento];
+  var nuevas_caracteristicas = '';
 
-var draw_characteristics = function(depa, desarollo) {
+  nuevas_caracteristicas = caracteristicas(departamento, desarrollo);
+  lista_de_caracteristicas.removeChild(lista_actual);
+  lista_de_caracteristicas.appendChild(nuevas_caracteristicas);
+  activar_elemento(el);
+  plano.src = '/static/img/vre/' + desarrollo + '/' + info_desarrollo.caracteristicas.plano;
 
-  var desarrollo = departamentos_json.departamentos[desarollo];
+};
+
+
+/*
+* Retorna el elemento UL.
+* Y sus elementos hijos LI ya con las caracteristicas.
+*/
+var caracteristicas = function(depa, desa) {
+
+  // Se obtiene toda la informacion del departamento.
+  // departamentos_json es parte del archivo info-departamentos.json.js
+  // Contiene en formato JSON toda la inforamcion.
+  var desarrollo      = departamentos_json.departamentos[desa];
   var caracteristicas = desarrollo[depa].caracteristicas;
 
-  var template_li = "";
-
+  // Se crean elementos de listam, con las caracteristicas del departamento.
+  var ul       = document.createElement('ul');
+  ul.className = 'animated pulse icons-list';
 
   for(var caracteristica in caracteristicas) {
-    console.log(caracteristicas[caracteristica]);
+    if (caracteristica != 'plano') {
+      var li  = document.createElement('li');
+      var img = document.createElement('img');
+      var p   = document.createElement('p');
 
-    template_li += "<li class='list-item'><img src='/static/vre/" + caracteristica + ".svg' /><p>" + caracteristicas[caracteristica] + "</p></li>";
+      li.className = 'list-item';
+      img.src      = caracteristica;
+      p.innerHTML  = caracteristicas[caracteristica];
+      
+      li.appendChild(img);
+      li.appendChild(p);
+      ul.appendChild(li);
+    }
   }
 
-  return template_li;
-}
-
-draw_characteristics("depa101", "beistegui");
+  return ul;
+};
